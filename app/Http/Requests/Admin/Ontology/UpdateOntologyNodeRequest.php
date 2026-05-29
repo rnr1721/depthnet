800ed036b15b18e955e1682f931586008dbc7686
Eugeny Gazzaev <rnr1721@gmail.com>
2026-05-29 16:02:09 +0300

    bugfix: ontology node update request

--- app/Http/Requests/Admin/Ontology/UpdateOntologyNodeRequest.php
+++ app/Http/Requests/Admin/Ontology/UpdateOntologyNodeRequest.php
@@ -7,9 +7,11 @@ class UpdateOntologyNodeRequest extends BaseOntologyRequest
     public function rules(): array
     {
         return array_merge($this->presetRules(), [
+            'canonical_name' => 'required|string|max:255',
             'class'    => 'required|string|max:50',
             'aliases'  => 'nullable|array',
             'aliases.*' => 'string|max:100',
+            'weight'         => 'required|numeric|min:0',
         ]);
     }
 }
